a18144b91fef5d7063c5764f4dc70199fcfefa6a
sayed <mdsayed.programmer@gmail.com>
2026-06-25 19:39:18 +0600

    auth_CLIENT

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,6 +1,5 @@
 import { createAuthClient } from "better-auth/react";
 
-// Safely format the Vercel system URL string if it exists
 const getBaseUrl = () => {
   if (process.env.NEXT_PUBLIC_BASE_URL) return process.env.NEXT_PUBLIC_BASE_URL;
   if (process.env.NEXT_PUBLIC_VERCEL_URL) return `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
